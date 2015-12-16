3d1a55b68064884d26755718b067ebf8f3597e91
Karen Hambro <khambro@gmail.com>
2015-12-16 15:55:10 -0800

    adds functions

--- shopping.js
+++ shopping.js
@@ -4,21 +4,45 @@ window.onload = function () {
   var lister = document.getElementById("lister");
   var item  = document.getElementById("item");
   var itemsDiv = document.getElementById("items");
+  var complete = document.getElementById("complete");
+  var listed = document.getElementById("listed");
+
 
   container.addEventListener("submit", function (event) {
     event.preventDefault();
     var ul = document.getElementById("listed");
     var item  = document.getElementById("item");
     var li = document.createElement("li");
+    var button= document.createElement("button");
+    li.setAttribute("id","li"); // added line
     li.appendChild(document.createTextNode(item.value));
-    // li.setAttribute("id", "element4"); // added line
+    button.className="complete";
+    button.id="complete"
+    button.innerHTML="✔";
+    li.appendChild(button);
     ul.appendChild(li);
-    item.value= ""
+    item.value = "";
+
+  });
+
+  itemsDiv.addEventListener("click", function (event) {
+    event.preventDefault();
+    var ul = document.getElementById("listed");
+    var li = document.getElementById("li");
+    li.style.color = 'gray';
+    ul.appendChild(li);
+
+
+
   });
 
 
 
 
+
+  // complete.addEventListener("submit", function (event))
+  //
+  //
 
 
 };
